44149d185f36533cdda3073ea41d88df81602f78
Norman Clarke <norman@njclarke.com>
2010-07-07 14:27:38 -0300

    Improve benchmark correctness.

--- bench.rb
+++ bench.rb
@@ -1,37 +1,39 @@
 # encoding: utf-8
 require "rubygems"
+require "bundler"
+Bundler.setup
+require "active_support"
 require "rbench"
 require File.expand_path("../init", __FILE__)
 
-SAMPLE = "Ja, żołnierz Wojska Polskiego, przysięgam służyć wiernie Rzeczypospolitej Polskiej".to_slug
 
-RBench.run(1_000) do
+def sample
+  "Ja, żołnierz Wojska Polskiego, przysięgam służyć wiernie Rzeczypospolitej Polskiej".to_slug
+end
+
+RBench.run(1000) do
 
   column :times
   column :time
 
   report 'Truncate bytes' do
-    time { SAMPLE.truncate_bytes(20) }
+    time { sample.truncate_bytes!(20) }
   end
 
   report 'Truncate chars' do
-    time { SAMPLE.truncate(20) }
+    time { sample.truncate!(20) }
   end
 
   report 'Approximate ASCII' do
-    time { SAMPLE.approximate_ascii }
+    time { sample.approximate_ascii! }
   end
 
   report 'Approximate with override' do
-    time { SAMPLE.approximate_ascii("ć" => "C") }
+    time { sample.approximate_ascii!("ć" => "C") }
   end
 
   report 'Strip non-ASCII' do
-    time { SAMPLE.to_ascii }
-  end
-
-  report 'Make truncated ASCII slug' do
-    time { SAMPLE.approximate_ascii.truncate_bytes(100).normalize }
+    time { sample.to_ascii! }
   end
 
 end
