89f53ed21c30cc8dacf25492017debcd2059d976
Denys Kurets <denys281@gmail.com>
2014-06-03 18:20:31 +0300

    ukrainian transliteration improve and add test

--- lib/babosa/transliterator/ukrainian.rb
+++ lib/babosa/transliterator/ukrainian.rb
@@ -3,10 +3,27 @@ module Babosa
   module Transliterator
     class Ukrainian < Cyrillic
       APPROXIMATIONS = {
+        "Г" => "H",
+        "г" => "h",
+        "Ґ" => "G",
+        "ґ" => "g",
+        "є" => "ie",
         "И" => "Y",
         "и" => "y",
         "І" => "I",
-        "і" => "i"
+        "і" => "i",
+        "ї" => "i",
+        "Й" => "Y",
+        "й" => "i",
+        "Х" => "Kh",
+        "х" => "kh",
+        "Ц" => "Ts",
+        "ц" => 'ts',
+        "Щ" => "Shch",
+        "щ" => "shch",
+        "ю" => "iu",
+        "я" => "ia",
+        "'" => ""
       }
     end
   end
