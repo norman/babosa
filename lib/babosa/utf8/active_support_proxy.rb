d89d3ccc7215706c22e62c04cd734b6968a7c024
Norman Clarke <norman@njclarke.com>
2015-01-05 10:45:33 -0300

    Simplify ActiveSupport proxy

--- lib/babosa/utf8/active_support_proxy.rb
+++ lib/babosa/utf8/active_support_proxy.rb
@@ -4,22 +4,10 @@ module Babosa
   module UTF8
     # A UTF-8 proxy using Active Support's multibyte support.
     module ActiveSupportProxy
-      extend Proxy
-      extend self
-      def downcase(string)
-        ActiveSupport::Multibyte::Unicode.downcase(string)
-      end
+      extend ActiveSupport::Multibyte::Unicode
 
-      def upcase(string)
-        ActiveSupport::Multibyte::Unicode.upcase(string)
-      end
-
-      def normalize_utf8(string)
-        ActiveSupport::Multibyte::Unicode.normalize(string)
-      end
-
-      def tidy_bytes(string)
-        ActiveSupport::Multibyte::Unicode.tidy_bytes(string)
+      def self.normalize_utf8(string)
+        normalize(string, :c)
       end
     end
   end
