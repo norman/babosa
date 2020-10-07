11e0f330d29163e56ab59c05977f5a611bf7caa1
Norman Clarke <norman@njclarke.com>
2020-10-07 11:21:58 -0300

    Replace STRIPPABLE codepoints array with regular expression

--- lib/babosa/identifier.rb
+++ lib/babosa/identifier.rb
@@ -1,16 +1,4 @@
 module Babosa
-  # Codepoints for characters that will be deleted by +#word_chars!+.
-  # rubocop:disable Layout/ArrayAlignment
-  STRIPPABLE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 37, 38, 39,
-    40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94,
-    96, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
-    137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151,
-    152, 153, 154, 155, 156, 157, 158, 159, 161, 162, 163, 164, 165, 166, 167,
-    168, 169, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 182, 183, 184,
-    185, 187, 188, 189, 190, 191, 215, 247, 8203, 8204, 8205, 8239, 65279].freeze
-  # rubocop:enable Layout/ArrayAlignment
-
   # Windows CP1252 codepoints mapped to Unicode bytes.
   CP1252 = {
     128 => [226, 130, 172],
@@ -152,10 +140,14 @@ module Babosa
     end
 
     # Remove any non-word characters. For this library's purposes, this means
-    # anything other than letters, numbers, spaces, newlines and linefeeds.
+    # anything other than letters, numbers, spaces, underscores, newlines and
+    # linefeeds.
     # @return String
     def word_chars!
-      @wrapped_string = (codepoints - Babosa::STRIPPABLE).pack("U*")
+      # `\P{L}` = Any non-Unicode letter
+      # `&&` = add the following character class
+      # `[^ _\n\r]` = Anything other than space, underscore, newline or linefeed
+      @wrapped_string = @wrapped_string.gsub(/[\P{L}&&[^ _\n\r]]/, "")
     end
 
     # Normalize the string for use as a URL slug. Note that in this context,
