38760249de0215aa35af5c27932efcc5a3f2d7b7
Stephen Rushe <srushe@users.noreply.github.com>
2022-12-22 13:06:18 +0000

    Remove unneeded requires

--- lib/babosa/transliterator/cyrillic.rb
+++ lib/babosa/transliterator/cyrillic.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "base"
-
 module Babosa
   module Transliterator
     # Approximations are based on GOST 7.79, System B:
--- lib/babosa/transliterator/greek.rb
+++ lib/babosa/transliterator/greek.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "base"
-
 module Babosa
   module Transliterator
     class Greek < Base
